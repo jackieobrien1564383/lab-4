7d0d23c4b6f57b408cd05cd9252a2ca707e44108
Jacqueline OBrien <1564383@students.latrobe.edu.au>
2024-08-26 18:11:27 +1000

    Completed Lab 4 React 1 and made a static React App

--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,29 @@
+function Task(props) {
+	return (
+			<li>{props.description} <input type="checkbox" checked={props.completed} readOnly /></li>
+	);
+}
+
 function List(props) {
+	// var taskArray = [];
+	// for (let i=0;i<props.tasks.length;i++)
+	// {	let desc = props.tasks[i].description;
+	// 	let stat= props.tasks[i].completed;
+	// 	taskArray.push(<Task description={desc} completed={stat}/>)
+	// }
+
 	return (
 		<div>
 			<h1>{ props.heading }</h1>
 			<ul>
-				<li>Learn React</li>
-				<li>Learn JSX</li>
-				<li>Build a React App</li>
+				{ props.tasks.map(task =>
+					<Task
+					description={task.description}
+					completed={task.completed} /> ) }
 			</ul>
 		</div>
 	);
+
 }
 
 export default List;
